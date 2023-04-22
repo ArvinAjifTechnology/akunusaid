465315f8b2559f14973c223bb668c0e0c8020788
ArvinAjifTechology <arvinajif3@gmail.com>
2023-04-22 16:58:31 +0700

    Menambahkan Template Company Profile Dan View Home

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Company\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('layouts.company.main');
+// });
+
+Route::resource('/', HomeController::class);
 
 Auth::routes();
 
