7e84d0e097e7b40f00310db1d0acf572bef54af7
ArvinAjifTechology <arvinajif3@gmail.com>
2023-04-25 16:50:04 +0700

    Mennambahkan Model City DKK

--- database/migrations/2023_04_24_012106_create_deliveries_table.php
+++ database/migrations/2023_04_24_012106_create_deliveries_table.php
@@ -13,12 +13,28 @@ return new class extends Migration
     {
         Schema::create('deliveries', function (Blueprint $table) {
             $table->id();
-            $table->string('shipper');
-            $table->string('shipper_addres');
-            $table->string('shipper_neighborhood');
-            $table->string('shipper_district');
-            $table->string('city_code');
-            $table->string('shipper_pos_code');
+            $table->string('nomor_resi')->nullable();
+            $table->string('shipper')->nullable();
+            // $table->foreignId('customer_id')->nullable();
+            $table->string('shipper_addres')->nullable();
+            $table->string('shipper_neighborhood')->nullable();
+            $table->string('shipper_district')->nullable();
+            $table->string('city_code')->nullable();
+            $table->string('shipper_pos_code')->nullable();
+            $table->string('destination')->nullable();
+            $table->string('destination_address')->nullable();
+            $table->string('destination_neighborhood')->nullable();
+            $table->string('destination_district')->nullable();
+            $table->string('destination_city_code')->nullable();
+            $table->string('destination_pos_code')->nullable();
+            $table->string('type_id')->nullable();
+            $table->foreignId('category_id')->nullable();
+            $table->bigInteger('qty')->nullable();
+            $table->string('weight')->nullable();
+            $table->double('cost', 15, 8)->nullable();
+            $table->foreignId('user_id')->nullable();
+            $table->timestamp('date_receiped')->nullable();
+            $table->timestamp('date_done')->nullable();
             $table->timestamps();
         });
     }
