9878ae50c1b20ad9175721d2dbc0f14e865b7fda
ArvinAjifTechology <arvinajif3@gmail.com>
2023-04-25 16:49:43 +0700

    Mengubah sStruktur Tabel

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,17 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->foreignid('customer_id')->nulllable();
+            $table->string('agen_id')->unique();
+            $table->string('username')->unique();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable;
+            $table->string('address');
+            $table->enum('is_admin', ['0', '1'])->nullable();
+            $table->string('region');
+            $table->string('phone_number');
             $table->rememberToken();
             $table->timestamps();
         });
