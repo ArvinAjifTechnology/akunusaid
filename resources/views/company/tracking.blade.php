d30fbb781d94d7c97bca6c36cf6f860a03ed287f
ArvinAjifTechology <arvinajif3@gmail.com>
2023-04-27 18:41:04 +0700

    Update Tracking

--- resources/views/company/tracking.blade.php
+++ resources/views/company/tracking.blade.php
@@ -37,50 +37,63 @@
 
         </div>
         <div class="row gy-4 d-flex justify-content-between">
-            <div class="col-lg-6 order-2 order-lg-1 d-flex flex-column justify-content-center">
-            <form action="#" class="form-search d-flex align-items-stretch mb-3" data-aos="fade-up" data-aos-delay="200">
-                <input type="text" class="form-control" placeholder="Masukan Nomor Resi">
-                <button type="submit" class="btn btn-primary">Search</button>
-            </form>
-            </div>
+
         </div>
-        <div class="row d-flex justify-content-between">
+        {{-- <div class="row d-flex justify-content-between">
             <div class="col-lg-4">Nomor Resi : 2023042300001</div>
             <div class="col-lg-4">Dari : Garut</div>
             <div class="col-lg-4">Tujuan : Jakarta</div>
-        </div>
+        </div> --}}
         <div class="row gy-4">
-            <table class="table table-primary table-hover">
-                <thead>
-                    <tr>
-                      <th scope="col">No</th>
-                      <th scope="col">Tanggal</th>
-                      <th scope="col">Status</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <th scope="row">1</th>
-                      <td>2023-04-20</td>
-                      <td>Diterima Agen Garut</td>
-                    </tr>
-                    <tr>
-                      <th scope="row">2</th>
-                      <td>2023-04-20</td>
-                      <td>Packing</td>
-                    </tr>
-                    <tr>
-                      <th scope="row">3</th>
-                      <td>2023-04-21</td>
-                      <td>Agen Bandung</td>
-                    </tr>
-                    <tr>
-                      <th scope="row">4</th>
-                      <td>2023-04-22</td>
-                      <td>Agen Jakarta</td>
-                    </tr>
-                  </tbody>
-            </table>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row">
+            <div class="col-lg-6 mt-5">
+                <div class="col-lg-12 mb-3 order-2 order-lg-1 d-flex flex-column justify-content-center">
+                    <form action="#" class="form-search d-flex align-items-stretch mb-3" data-aos="fade-up" data-aos-delay="200">
+                        <input type="text" class="form-control" placeholder="Masukan Nomor Resi">
+                        <button type="submit" class="btn btn-coklat">Track</button>
+                    </form>
+                    </div>
+                <div class="col-lg-6 tracking-box">Nomor Resi : 2023062300001</div>
+                <div class="col-lg-6 tracking-box mt-5">Dari : Garut</div>
+                <div class="col-lg-6 tracking-box mt-3">Tujuan : Jakarta</div>
+            </div>
+            <div class="col-lg-6 mt-5">
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                          <th scope="col">No</th>
+                          <th scope="col">Tanggal</th>
+                          <th scope="col">Status</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <th scope="row">1</th>
+                          <td>2023-04-20</td>
+                          <td>Diterima Agen Garut</td>
+                        </tr>
+                        <tr>
+                          <th scope="row">2</th>
+                          <td>2023-04-20</td>
+                          <td>Packing</td>
+                        </tr>
+                        <tr>
+                          <th scope="row">3</th>
+                          <td>2023-04-21</td>
+                          <td>Agen Bandung</td>
+                        </tr>
+                        <tr>
+                          <th scope="row">4</th>
+                          <td>2023-04-22</td>
+                          <td>Agen Jakarta</td>
+                        </tr>
+                      </tbody>
+                </table>
+            </div>
+        </div>
       </div>
     </section><!-- End Frequently Asked Questions Section -->
 
