d3f1f4f512d8cb334301f042909b36e68a2fe27d
ArvinAjifTechology <arvinajif3@gmail.com>
2023-04-27 20:36:49 +0700

    Update Contact

--- resources/views/company/contact.blade.php
+++ resources/views/company/contact.blade.php
@@ -28,7 +28,7 @@
 
     <!-- ======= Contact Section ======= -->
     <section id="contact" class="contact">
-      <div class="container" data-aos="fade-up">
+      <div class="container" data-aos="fade-up" data-aos-delay="100">
 
         <div>
           <!-- <iframe style="border:0; width: 100%; height: 340px;" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" frameborder="0" allowfullscreen></iframe> -->
@@ -41,15 +41,15 @@
 
           <div class="col-lg-4">
 
-            <div class="info-item d-flex">
-              <i class="bi bi-geo-alt flex-shrink-0"></i>
+            <div class="info-item d-flex" data-aos="fade-up">
+              <i class="bi bi-geo-alt flex-shrink-0" data-aos="fade-up"></i>
               <div>
                 <h4>Location:</h4>
                 <p>A108 Adam Street, New York, NY 535022</p>
               </div>
             </div><!-- End Info Item -->
 
-            <div class="info-item d-flex">
+            <div class="info-item d-flex" data-aos="fade-up">
               <i class="bi bi-envelope flex-shrink-0"></i>
               <div>
                 <h4>Email:</h4>
@@ -57,7 +57,7 @@
               </div>
             </div><!-- End Info Item -->
 
-            <div class="info-item d-flex">
+            <div class="info-item d-flex" data-aos="fade-up">
               <i class="bi bi-phone flex-shrink-0"></i>
               <div>
                 <h4>Call:</h4>
@@ -76,25 +76,25 @@
             <form action="{{route('send.message') }}" method="post" role="form" class="">
                 @csrf
               <div class="row">
-                <div class="col-md-6 form-group">
+                <div class="col-md-6 form-group" data-aos="fade-up">
                   <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
                 </div>
-                <div class="col-md-6 form-group mt-3 mt-md-0">
+                <div class="col-md-6 form-group mt-3 mt-md-0" data-aos="fade-up">
                   <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required>
                 </div>
               </div>
-              <div class="form-group mt-3">
+              <div class="form-group mt-3" data-aos="fade-up">
                 <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required>
               </div>
-              <div class="form-group mt-3">
+              <div class="form-group mt-3" data-aos="fade-up">
                 <textarea class="form-control" name="message" rows="5" placeholder="Message" required></textarea>
               </div>
-              <div class="my-3">
+              <div class="my-3"data-aos="fade-up">
                 {{-- <div class="loading">Loading</div>
                 {{-- <div class="error-message"></div>
                 <div class="sent-message">Your message has been sent. Thank you!</div>
               </div> --}}
-              <div class="text-center"><button type="submit" class="btn btn-coklat">Send Message</button></div>
+              <div class="text-center" data-aos="fade-up"><button type="submit" class="btn btn-coklat">Send Message</button></div>
             </form>
           </div><!-- End Contact Form -->
 
