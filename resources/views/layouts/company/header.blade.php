7d1145d1d2133146f1727cc6afb5310aaf3585dc
ArvinAjifTechology <arvinajif3@gmail.com>
2023-04-27 16:13:34 +0700

    Update Header

--- resources/views/layouts/company/header.blade.php
+++ resources/views/layouts/company/header.blade.php
@@ -3,8 +3,8 @@
 
       <a href="index.html" class="logo d-flex align-items-center">
         <!-- Uncomment the line below if you also wish to use an image logo -->
-        <!-- <img src="assets/img/logo.png" alt=""> -->
-        <h1>AkuNusaExpess</h1>
+        <img src="{{ asset('company') }}/assets/img/Logo Akunusa Express.png" alt="">
+        {{-- <h1>AkuNusaExpess</h1> --}}
       </a>
 
       <i class="mobile-nav-toggle mobile-nav-show bi bi-list"></i>
@@ -12,15 +12,15 @@
       <nav id="navbar" class="navbar">
         <ul>
           <li><a href="/" class="{{ Request::is('/') ? 'active' : '' }}">Home</a></li>
-          <li><a href="/about" class="{{ Request::is('/about') ? 'active' : '' }}">About</a></li>
-          <li><a href="/agen-list" class="{{ Request::is('/agen-list') ? 'active' : '' }}">Agen List</a></li>
-          <li><a href="/services" class="{{ Request::is('/services') ? 'active' : '' }}">Services</a></li>
-          <li><a href="/pricing" class="{{ Request::is('/pricing') ? 'active' : '' }}">Pricing</a></li>
-          <li><a href="/tracking" class="{{ Request::is('/tracking') ? 'active' : '' }}">Tracking</a></li>
-          <li><a href="/contact" class="{{ Request::is('/contact') ? 'active' : '' }}">Contact</a></li>
-          <li><a class="get-a-quote" href="/login"  class="{{ Request::is('/login') ? 'active' : '' }}">Login</a></li>
+          <li><a href="/about" class="{{ Request::is('about') ? 'active' : '' }}">About</a></li>
+          <li><a href="/agen-list" class="{{ Request::is('agen-list') ? 'active' : '' }}">Agen List</a></li>
+          <li><a href="/services" class="{{ Request::is('services') ? 'active' : '' }}">Services</a></li>
+          <li><a href="/pricing" class="{{ Request::is('pricing') ? 'active' : '' }}">Pricing</a></li>
+          <li><a href="/tracking" class="{{ Request::is('tracking') ? 'active' : '' }}">Tracking</a></li>
+          <li><a href="/contact" class="{{ Request::is('contact') ? 'active' : '' }}">Contact</a></li>
+          <li><a class="get-a-quote" href="/login"  class="{{ Request::is('login') ? 'active' : '' }}">Login</a></li>
         </ul>
       </nav><!-- .navbar -->
 
     </div>
-  </header><!-- End Header -->
+</header><!-- End Header -->
