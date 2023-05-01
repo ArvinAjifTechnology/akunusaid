d5df290dc67ded4a2b9a193d5c28b546e23afd6e
ArvinAjifTechology <arvinajif3@gmail.com>
2023-05-01 13:50:05 +0700

    Update Header

--- resources/views/layouts/company/header.blade.php
+++ resources/views/layouts/company/header.blade.php
@@ -11,14 +11,14 @@
       <i class="mobile-nav-toggle mobile-nav-hide d-none bi bi-x"></i>
       <nav id="navbar" class="navbar">
         <ul>
-          <li><a href="/" class="{{ Request::is('/') ? 'active' : '' }}">Home</a></li>
-          <li><a href="/about" class="{{ Request::is('about') ? 'active' : '' }}">About</a></li>
-          <li><a href="/agen-list" class="{{ Request::is('agen-list') ? 'active' : '' }}">Agen List</a></li>
-          <li><a href="/services" class="{{ Request::is('services') ? 'active' : '' }}">Services</a></li>
-          <li><a href="/pricing" class="{{ Request::is('pricing') ? 'active' : '' }}">Pricing</a></li>
-          <li><a href="/tracking" class="{{ Request::is('tracking') ? 'active' : '' }}">Tracking</a></li>
-          <li><a href="/contact" class="{{ Request::is('contact') ? 'active' : '' }}">Contact</a></li>
-          <li><a class="get-a-quote" href="/login"  class="{{ Request::is('login') ? 'active' : '' }}">Login</a></li>
+          <li><a href="{{ url('/') }}" class="{{ Request::is('/') ? 'active' : '' }}">Home</a></li>
+          <li><a href="{{ url('/about') }}" class="{{ Request::is('about') ? 'active' : '' }}">About</a></li>
+          <li><a href="{{ url('/agen-list') }}" class="{{ Request::is('agen-list') ? 'active' : '' }}">Agen List</a></li>
+          <li><a href="{{ url('/services') }}" class="{{ Request::is('services') ? 'active' : '' }}">Services</a></li>
+          <li><a href="{{ url('/pricing') }}" class="{{ Request::is('pricing') ? 'active' : '' }}">Pricing</a></li>
+          <li><a href="{{ url('/tracking') }}" class="{{ Request::is('tracking') ? 'active' : '' }}">Tracking</a></li>
+          <li><a href="{{ url('/contact') }}" class="{{ Request::is('contact') ? 'active' : '' }}">Contact</a></li>
+          <li><a class="get-a-quote" href="{{ url('/login') }}"  class="{{ Request::is('login') ? 'active' : '' }}">Login</a></li>
         </ul>
       </nav><!-- .navbar -->
 
